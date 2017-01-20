05e28bdfe6fdc50bc45383fdad877bc9feeeda2b
Aniket Munshi <Aniket Munshi>
2017-01-20 14:05:41 +0530

    Update method corrected

--- reactApp/components/AddTodo.jsx
+++ reactApp/components/AddTodo.jsx
@@ -2,14 +2,14 @@ import React, {Component, PropTypes} from 'react'
 
 
 export default class AddTodo extends Component{
-    render(){
-    	update(e){
-	        const node = this.refs.input
-    	 	const text = node.value.trim()
-     		this.props.onAddClick(text)
-    		node.value="Add Todo Item"
-		}
+   	update(e){
+        const node = this.refs.input
+   	 	const text = node.value.trim()
+   		this.props.onAddClick(text)
+   		node.value="Add Todo Item"
+	}
 
+    render(){
         return(
             <div>
                 <input type="text" ref="input" />
